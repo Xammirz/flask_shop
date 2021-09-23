69f0665dec755a96aaf1d487b7d491d2d3b0861d
Xammirzaev <ramzanxam95reg@gmail.com>
2021-09-23 18:06:33 +0300

    ramzan

---
+++ config.py
@@ -0,0 +1,4 @@
+
+class Config():
+    SQLAlCHEMY_DATABASE_URI = 'sqlite:///shop.db'
+    SQALCHEMY_TRACK_MODIFICATION = False
--- shop/__init__.py
+++ shop/__init__.py
@@ -1,5 +1,10 @@
+from flask.config import Config
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-
+from config import Config
+from flask_migrate import Migrate
 app = Flask(__name__)
-
-from shop  import routes
+db = SQLAlchemy(app)
+app.config.from_object(Config)
+migrate = Migrate(app , db)
+from shop  import routes, models
